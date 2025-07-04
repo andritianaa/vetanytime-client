4cb7485fc68010bd5902bb1083e2357ffd2059cd
andritianaa <andritianasteve@gmail.com>
2025-07-04 11:12:58 +0300

    feat: update application name and change port configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     apps: [
         {
-            name: "Vetanytime prod",
+            name: "Vetanytime client",
             script: "npm",
             args: "start",
             env: {
-                PORT: 3000,
+                PORT: 3042,
                 NODE_ENV: "production"
             }
         }
